fe6e80a1dad4a2260eeb1052aca3538a2ea3a938
adilthesav <adilthesav@gmail.com>
2026-08-29 01:01:17 -0500

    Update OwnerBlogPanel.tsx

--- src/app/components/OwnerBlogPanel.tsx
+++ src/app/components/OwnerBlogPanel.tsx
@@ -455,13 +455,11 @@ export function OwnerBlogPanel() {
       )}
 
       {/* Toast */}
-      {toast && (
+          {toast && (
         <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-50 bg-stone-900 text-white text-sm font-medium px-5 py-3 rounded-full shadow-xl">
           {toast}
         </div>
       )}
     </div>
-  );
-}
   );
 }
